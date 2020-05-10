172bc1fb9eaf648ef58158f33d70338b24846f49
Justin Yoon <justinyoondev@gmail.com>
2020-05-10 20:47:36 +1200

    add install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#install zsh
+sudo apt install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install fish-like autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+# update plugins list
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions history-substring-search)/' ~/.zshrc
+
